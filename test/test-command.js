552289764b94a287117e88b54f0c15a62065180a
Edward Hahn <ehahn33@gmail.com>
2014-08-08 02:37:31 -0600

    Made JIRA creds persistent and stored in .env.

--- test/test-command.js
+++ test/test-command.js
@@ -11,6 +11,33 @@ var testConfigPath = 'test/fixtures/kosher.json';
 
 var bin = path.join(path.dirname(fs.realpathSync(__filename)), '../bin/');
 
+function cmd(command, callback, streamOutput){
+    if(streamOutput){
+        var procs = command.split(' ');
+        var proc = spawn(procs[0], (procs.length > 1)?procs.slice(1, 1+procs.length-1):[]);
+
+        proc.stdout.on('data', function (data) {
+            console.log(data.toString());
+        });
+
+        proc.stderr.on('data', function (data) {
+            console.log('stderr: ' + data);
+        });
+
+        proc.on('exit', function (code) {
+            console.log('child process exited with code ' + code);
+
+            callback(code);
+        });
+
+        
+    } else {
+        exec(command, function (error, stdout, stderr) {
+            callback(error?error.code:0);
+        });
+    }
+}
+
 describe('Sync Tests', function(){
 
     // before(function(done){
@@ -29,24 +56,24 @@ describe('Sync Tests', function(){
 
     describe('#command', function() {
         it('should not allow --feature and --scenario to both be specified', function(done){
-            child = exec(bin+'jira-kosher --feature KOSH-1 --scenario KOSH-3', function (error, stdout, stderr) {
-                (error != null).should.be.ok;
+            cmd(bin+'jira-kosher --feature KOSH-1 --scenario KOSH-3', function(result){
+                result.should.not.equal(0);
 
                 done();
             });
         });
 
         it('should not allow --feature and --createBranch to both be specified', function(done){
-            child = exec(bin+'jira-kosher --feature KOSH-1 --createBranch', function (error, stdout, stderr) {
-                (error != null).should.be.ok;
+            cmd(bin+'jira-kosher --feature KOSH-1 --createBranch', function(result){
+                result.should.not.equal(0);
 
                 done();
             });
         });
 
         it('should not allow --createBranch without <scenario_key>', function(done){
-            child = exec(bin+'jira-kosher --createBranch', function (error, stdout, stderr) {
-                (error != null).should.be.ok;
+            cmd(bin+'jira-kosher --createBranch', function(result){
+                result.should.not.equal(0);
 
                 done();
             });
@@ -55,89 +82,27 @@ describe('Sync Tests', function(){
 
     describe('#sync', function() {
         it('should sync features from JIRA', function(done){
-            var kosher = spawn(bin+'jira-kosher', [
-                '--username', '***REMOVED***',
-                '--password', '***REMOVED***'
-            ]);
-
-            kosher.stdout.on('data', function (data) {
-                console.log(data.toString());
-            });
-
-            kosher.stderr.on('data', function (data) {
-                console.log('stderr: ' + data);
-            });
-
-            kosher.on('exit', function (code) {
-                console.log('child process exited with code ' + code);
-                code.should.equal(0);
+            cmd(bin+'jira-kosher', function(result){
+                result.should.equal(0);
 
                 done();
             });
-
-            // child = exec(bin+'jira-kosher --username ***REMOVED*** --password ***REMOVED***', function (error, stdout, stderr) {
-            //     (error == null).should.be.ok;
-
-            //     done();
-            // });
         });
 
-        it('should sync a single feature from JIRA', function(done){
-            var kosher = spawn(bin+'jira-kosher', [
-                '--username', '***REMOVED***',
-                '--password', '***REMOVED***',
-                '--feature', 'KOSH-1'
-            ]);
-
-            kosher.stdout.on('data', function (data) {
-                console.log(data.toString());
-            });
-
-            kosher.stderr.on('data', function (data) {
-                console.log('stderr: ' + data);
-            });
-
-            kosher.on('exit', function (code) {
-                console.log('child process exited with code ' + code);
-                code.should.equal(0);
+        it.skip('should sync a single feature from JIRA', function(done){
+            cmd(bin+'jira-kosher --feature KOSH-1', function(result){
+                result.should.equal(0);
 
                 done();
             });
-
-            // child = exec(bin+'jira-kosher --username ***REMOVED*** --password ***REMOVED***', function (error, stdout, stderr) {
-            //     (error == null).should.be.ok;
-
-            //     done();
-            // });
         });
 
         it.skip('should sync a single scenario from JIRA', function(done){
-            var kosher = spawn(bin+'jira-kosher', [
-                '--username', '***REMOVED***',
-                '--password', '***REMOVED***',
-                '--scenario', 'KOSH-3'
-            ]);
-
-            kosher.stdout.on('data', function (data) {
-                console.log(data.toString());
-            });
-
-            kosher.stderr.on('data', function (data) {
-                console.log('stderr: ' + data);
-            });
-
-            kosher.on('exit', function (code) {
-                console.log('child process exited with code ' + code);
-                code.should.equal(0);
+            cmd(bin+'jira-kosher --scenario KOSH-3', function(result){
+                result.should.equal(0);
 
                 done();
             });
-
-            // child = exec(bin+'jira-kosher --username ***REMOVED*** --password ***REMOVED***', function (error, stdout, stderr) {
-            //     (error == null).should.be.ok;
-
-            //     done();
-            // });
         });
     });
 });
--- test/test-sync.js
+++ test/test-sync.js
@@ -35,20 +35,8 @@ describe('Sync Tests', function(){
             });
         });
 
-        it.skip('should sync features from JIRA without prompting for credentials', function(done){
-            process.env.JIRA_USERNAME = '***REMOVED***';
-            process.env.JIRA_PASSWORD = '***REMOVED***';
-
-            Kosher.sync(testConfigPath, function(result){
-                result.should.equal(0);
-
-                done();
-            });
-        });
 
         it.skip('should sync only a single feature', function(done){
-            process.env.JIRA_USERNAME = '***REMOVED***';
-            process.env.JIRA_PASSWORD = '***REMOVED***';
             process.env.FEATURE_KEY = 'KOSH-1';
 
             Kosher.sync(testConfigPath, function(result){
@@ -59,8 +47,6 @@ describe('Sync Tests', function(){
         });
 
         it.skip('should sync only a single scenario', function(done){
-            process.env.JIRA_USERNAME = '***REMOVED***';
-            process.env.JIRA_PASSWORD = '***REMOVED***';
             process.env.SCENARIO_KEY = 'KOSH-3';
 
             Kosher.sync(testConfigPath, function(result){
